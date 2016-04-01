a88504fd6a6159ba3d41db3dc7c2bb5680d95a83
Tobi <fagerberg.tobias@gmail.com>
2016-04-01 23:56:09 +0200

    Add instructions on how to write events

--- src/js/event/events.js
+++ src/js/event/events.js
@@ -1,3 +1,10 @@
+// When creating events, name them with all CAPS separated with underscores.
+// Example: AMAZING_MAP_CREATED
+//
+// The value of the event should be a very short description on when the event
+// is fired.
+// Example: "when an awesome map was created"
+
 export default const Events = {
 	
 }
