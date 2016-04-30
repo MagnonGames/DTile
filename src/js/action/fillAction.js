e49302d07e863a65f6a226c8ce39d61a92e076be
Tobi <fagerberg.tobias@gmail.com>
2016-04-30 19:41:18 +0200

    Add basic fill action

---
+++ src/js/action/fillAction.js
@@ -0,0 +1,61 @@
+import DragPaintAction from "./dragPaintAction.js";
+
+export default class FillAction extends DragPaintAction {
+	constructor(x, y, layerId, tileArea) {
+		super(x, y, layerId, tileArea);
+
+		this.x = x;
+		this.y = y;
+		this.layerId = layerId;
+	}
+
+	apply(map) {
+		let getTile = map.tileLayers[this.layerId].getTile
+				.bind(map.tileLayers[this.layerId]),
+			fillId = getTile(this.x, this.y).id,
+			seeds = [{ x: this.x, y: this.y }];
+
+		let testSeed = (x, y, verticalModifier) => {
+			if (y + verticalModifier >= 0 &&
+				getTile(x, y + verticalModifier).id == fillId) {
+				if (x <= 0
+					|| getTile(x - 1, y + verticalModifier).id != fillId
+					|| getTile(x - 1, y).id != fillId) {
+					seeds.push({
+						x: x, y: y + verticalModifier
+					});
+				}
+			}
+		}
+
+		do {
+			let seed = seeds[0],
+				x = seed.x,
+				y = seed.y;
+
+			if (getTile(x, y).id != fillId) {
+				seeds.splice(0, 1);
+				continue;
+			}
+
+			do {
+				this.paint(x, y, map);
+				testSeed(x, y, 1);
+				testSeed(x, y, -1);
+				x++;
+			} while (x < map.width && getTile(x, y).id == fillId);
+
+			x = seed.x;
+			y = seed.y;
+			while (x > 0 && getTile(x - 1, y).id == fillId) {
+				x--;
+				this.paint(x, y, map);
+				testSeed(x, y, 1);
+				testSeed(x, y, -1);
+			}
+
+			seeds.splice(0, 1);
+			super.apply(map);
+		} while (seeds.length > 0);
+	}
+}
