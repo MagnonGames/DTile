b71f3fe54d5a4e341e0f7e32ad0988615327b735
Ellie <fagerberg.tobias@gmail.com>
2016-10-31 16:51:38 +0100

    Fix email for deploy

--- .deploy-gh.sh
+++ .deploy-gh.sh
@@ -3,7 +3,7 @@
 cd build/
 git init
 git config user.name "Travis-CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "travis@magnon.net"
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
