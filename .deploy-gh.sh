f6e4816df9db8fdb0b9618470231d23899a8178b
Ellie <ellie@magnon.net>
2017-07-12 15:40:43 +0200

    Remove CNAME

--- .deploy-gh.sh
+++ .deploy-gh.sh
@@ -4,7 +4,6 @@ cd build/
 git init
 git config user.name "Travis-CI"
 git config user.email "travis@magnon.net"
-cp ../.CNAME ./CNAME
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
