ce66a7562231a055b50d697ea4910c59aa1f2f8b
Ellie <ellie@magnon.net>
2017-08-29 08:27:45 +0200

    Make navigation more robust

--- src/dtile-state/store.js
+++ src/dtile-state/store.js
@@ -11,7 +11,16 @@ DTile.createStore = () => {
         reducers,
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
-    DTile.ReduxMixin = window.PolymerRedux(DTile.store);
+    DTile.ReduxMixin = superclass =>
+        window.PolymerRedux(DTile.store)(class extends superclass {
+            static get actions() {
+                return {
+                    setCurrentPage: page => ({ type: "SET_CURRENT_PAGE", payload: page }),
+                    setCurrentProjectId: id => ({ type: "SET_CURRENT_PROJECT_ID", payload: id }),
+                    setCurrentMapId: id => ({ type: "SET_CURRENT_MAP_ID", payload: id })
+                };
+            }
+        });
 };
 
 DTile.createStore();
