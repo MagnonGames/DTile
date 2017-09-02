ea8ebcaa7d31dc92ac612b0452d53c7f479a9c80
Ellie <ellie@magnon.net>
2017-09-02 17:15:48 +0200

    Tidy up state

--- src/dtile-state/store.js
+++ src/dtile-state/store.js
@@ -1,36 +1,34 @@
-DTile.createStore = () => {
-    const { createStore, combineReducers } = window.Redux;
-
-    const entities = combineReducers(DTile.reducers.entities);
-    // const reducers = combineReducers({
-    //     entities,
-    //     ui: DTile.reducers.ui
-    // });
-
-    const reducers = (state = {}, action) => {
-        if (action.type === "REPLACE") return action.payload;
-        else {
-            return {
-                entities: entities(state.entities, action),
-                ui: DTile.reducers.ui(state.ui, action)
-            };
-        }
+(() => {
+    const replaceable = reducer => {
+        return (state = {}, action) => {
+            if (action.type === "REPLACE") return action.payload;
+            else return reducer(state, action);
+        };
     };
 
-    DTile.store = createStore(
-        reducers,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
-    DTile.ReduxMixin = superclass =>
-        window.PolymerRedux(DTile.store)(class extends superclass {
-            static get actions() {
-                return {
-                    setCurrentPage: page => ({ type: "SET_CURRENT_PAGE", payload: page }),
-                    setCurrentProjectId: id => ({ type: "SET_CURRENT_PROJECT_ID", payload: id }),
-                    setCurrentMapId: id => ({ type: "SET_CURRENT_MAP_ID", payload: id })
-                };
-            }
-        });
-};
+    DTile.createStore = () => {
+        const { createStore, combineReducers } = window.Redux;
 
-DTile.createStore();
+        const reducers = replaceable(combineReducers({
+            entities: combineReducers(DTile.reducers.entities),
+            ui: DTile.reducers.ui
+        }));
+
+        DTile.store = createStore(
+            reducers,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        );
+        DTile.ReduxMixin = superclass =>
+            window.PolymerRedux(DTile.store)(class extends superclass {
+                static get actions() {
+                    return {
+                        setCurrentPage: page => ({ type: "SET_CURRENT_PAGE", payload: page }),
+                        setCurrentProjectId: id => ({ type: "SET_CURRENT_PROJECT_ID", payload: id }),
+                        setCurrentMapId: id => ({ type: "SET_CURRENT_MAP_ID", payload: id })
+                    };
+                }
+            });
+    };
+
+    DTile.createStore();
+})();
