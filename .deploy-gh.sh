f5083db8d9c7f01ab8c65a9fc65a9eaf5f6ce149
Ellie <ellie@magnon.net>
2017-09-25 08:16:56 +0200

    Change ci email

--- .deploy-gh.sh
+++ .deploy-gh.sh
@@ -3,7 +3,7 @@
 cd build/
 git init
 git config user.name "Travis-CI"
-git config user.email "travis@magnon.net"
+git config user.email "ci@magnon.net"
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
